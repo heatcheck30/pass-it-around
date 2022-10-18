9013831ee26c40a7ee5b5f26507c6d56731fe4fc
Winston Tang <winstontang@Winstons-MacBook-Pro.local>
2022-10-17 18:22:34 -0700

    feat: Completed pass-it-around portion of Express Lab 2

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req,res) => {
+    res.send(`<h1>99 Bottles of beer on the wall</h1>
+   <a href="/98">take one down, pass it around</a>
+    `)
+})
+
+app.get("/:number_of_bottles", (req,res) => {
+    let numberOfBottles = req.params.number_of_bottles 
+    if(numberOfBottles > 0 ) {
+      res.send(`<h1>${numberOfBottles} Bottles on wall</h1>
+      <a href= "/${numberOfBottles - 1}">take one down, pass it around</a>`);
+  
+    }else {
+     res.send(`<h1>Hand over your keys! <br> <a href = "/"> Start over</a> </h1>`);
+    }
+  
+    
+  
+  }
+  )
+  
+app.listen(port, () => {
+    console.log('listening!');
+});
